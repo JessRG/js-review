77794b83aea924695213585e555d39ae11f5bccb
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-13 00:26:46 -0500

    Refresher on fetching movies api in js. Need to learn how to use async await with an api.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+"use strict";
+
+import { MOVIE_KEY } from './keys.js';
+
+const img = document.getElementById("moviePoster");
+
+const info = () => {
+    return fetch(`http://www.omdbapi.com/?apikey=${MOVIE_KEY}&s=Guardians+Of+The+Galaxy`);
+}
+info().then(response => response.json()).then(response => {
+    img.setAttribute("src", response.Search[0].Poster);
+});
+
 const mainHeader = document.getElementById("mainHeader");
 
 const simpleHandler = () => {
     mainHeader.textContent = "This is the Header for the Index page";
 };
-
 mainHeader.addEventListener('load', simpleHandler());
