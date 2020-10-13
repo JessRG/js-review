ca3b9d9ee10fed027ce5bc7756c9970195a724f7
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-13 00:50:39 -0500

    Made an async arrow function from previous fetch.then chained function.

--- js/index.js
+++ js/index.js
@@ -4,11 +4,28 @@ import { MOVIE_KEY } from './keys.js';
 
 const img = document.getElementById("moviePoster");
 
-const info = () => {
-    return fetch(`http://www.omdbapi.com/?apikey=${MOVIE_KEY}&s=Guardians+Of+The+Galaxy`);
-}
-info().then(response => response.json()).then(response => {
-    img.setAttribute("src", response.Search[0].Poster);
+// const info = () => {
+//     return fetch(`http://www.omdbapi.com/?apikey=${MOVIE_KEY}&s=Guardians+Of+The+Galaxy`)
+//         .then(response => response.json());
+// }
+// info().then(response => {
+//     img.setAttribute("src", response.Search[0].Poster);
+// });
+
+const info = async () => {
+    let response = await fetch(`http://www.omdbapi.com/?apikey=${MOVIE_KEY}&s=Guardians+Of+The+Galaxy`);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+    } else {
+        let result = await response.json();
+        let poster = result.Search[0].Poster;
+        img.setAttribute("src", poster);
+    }
+};
+
+info().catch(e => {
+    console.log("There has been a problem with your fetch operation: " + e.message);
 });
 
 const mainHeader = document.getElementById("mainHeader");
