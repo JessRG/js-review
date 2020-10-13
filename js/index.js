ab88e70494a30549d105c63b409e72b1ad6d80a5
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-12 20:33:08 -0500

    Added an event listener to the h1 tag on index.html need to refresh with callbacks again with addEventListener.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-const
+const mainHeader = document.getElementById("mainHeader");
+
+const simpleHandler = () => {
+    mainHeader.textContent = "This is the Header for the Index page";
+};
+
+mainHeader.addEventListener('load', simpleHandler());
